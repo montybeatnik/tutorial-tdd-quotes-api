24b50547a8c95a85ca0a5043fb4da84e290fcef3
Chris Hern <christopher.t.hern@gmail.com>
2023-04-30 14:09:13 -0500

    feat: added support for postgres
    At the moment, we only allow:
     - Create
     - ByID
     - All
    
    Tests passing:
    go test ./...
    ok      github.com/montybeatnik/tutorials/tdd-quotes-api/cmd/quotes     0.188s
    ok      github.com/montybeatnik/tutorials/tdd-quotes-api/store  0.179s

---
+++ store/postgres.go
@@ -0,0 +1,84 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+	"time"
+)
+
+var (
+	ErrNoAuthor  = errors.New("must provide author")
+	ErrNoMessage = errors.New("must provide message")
+)
+
+type PGStore struct{ db *sql.DB }
+
+func NewPGStore(db *sql.DB) *PGStore {
+	return &PGStore{db: db}
+}
+
+func (ps *PGStore) Create(quote Quote) error {
+	if quote.Author == "" {
+		return ErrNoAuthor
+	}
+	if quote.Message == "" {
+		return ErrNoMessage
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	query := `INSERT INTO quotes (author, message) VALUES ($1, $2);`
+	stmt, err := ps.db.PrepareContext(ctx, query)
+	if err != nil {
+		return fmt.Errorf("couldn't prepare statement: %v", err)
+	}
+	_, err = stmt.ExecContext(ctx, quote.Author, quote.Message)
+	if err != nil {
+		return fmt.Errorf("couldn't add quote to DB: %v", err)
+	}
+	return nil
+}
+
+func (ps *PGStore) ByID(id int) (Quote, error) {
+	var quote Quote
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	query := `SELECT id, author, message FROM quotes WHERE id = $1;`
+	stmt, err := ps.db.PrepareContext(ctx, query)
+	if err != nil {
+		return quote, fmt.Errorf("couldn't prepare statement: %v", err)
+	}
+	if err := stmt.QueryRow(id).Scan(&quote.ID, &quote.Author, &quote.Message); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return quote, sql.ErrNoRows
+		}
+		return quote, fmt.Errorf("couldn't scan row into quote: %v", err)
+	}
+	return quote, nil
+}
+
+func (ps *PGStore) All() ([]Quote, error) {
+	var quotes []Quote
+	query := `SELECT id, author, message FROM quotes;`
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	stmt, err := ps.db.PrepareContext(ctx, query)
+	if err != nil {
+		return quotes, fmt.Errorf("couldn't prepare statement: %v", err)
+	}
+	rows, err := stmt.QueryContext(ctx)
+	if err != nil {
+		return quotes, fmt.Errorf("couldn't get rows: %v", err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var q Quote
+		if err := rows.Scan(&q.ID, &q.Author, &q.Message); err != nil {
+			log.Println("scan failed:", err)
+		}
+		quotes = append(quotes, q)
+	}
+	return quotes, nil
+}
--- store/postgres_test.go
+++ store/postgres_test.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"os"
 	"testing"
-	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -24,7 +23,7 @@ func getDB(t *testing.T) *sql.DB {
 }
 
 func createTable(t *testing.T, db *sql.DB) {
-	query := `CREATE TABLE IF NOT EXISTS quotes(id SERIAL PRIMARY KEY, author TEXT UNIQUE, message TEXT UNIQUE);`
+	query := `CREATE TABLE IF NOT EXISTS quotes(id SERIAL PRIMARY KEY, author TEXT UNIQUE NOT NULL, message TEXT UNIQUE NOT NULL);`
 	stmt, err := db.Prepare(query)
 	if err != nil {
 		t.Fatal(err)
@@ -35,15 +34,95 @@ func createTable(t *testing.T, db *sql.DB) {
 	}
 }
 
+func seed(t *testing.T, db *sql.DB) {
+	query := `INSERT INTO quotes (author, message) VALUES ('ghandi', 'be the change');`
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = stmt.Exec()
+	if err != nil {
+		t.Error(err)
+	}
+}
+
 func cleanup(t *testing.T, db *sql.DB) {
 	t.Cleanup(func() {
 		db.Exec(`DROP TABLE quotes;`)
 	})
 }
 
-func TestSetup(t *testing.T) {
+func TestCreateQuote(t *testing.T) {
+	testCases := []struct {
+		desc  string
+		quote Quote
+		err   error
+	}{
+		{
+			desc:  "create no author",
+			quote: Quote{Message: "be the change"},
+			err:   ErrNoAuthor,
+		},
+		{
+			desc:  "create no message",
+			quote: Quote{Author: "ghandi"},
+			err:   ErrNoMessage,
+		},
+		{
+			desc:  "create success",
+			quote: Quote{Author: "ghandi", Message: "be the change"},
+			err:   nil,
+		},
+	}
 	db := getDB(t)
 	createTable(t, db)
-	time.Sleep(20 * time.Second)
-	cleanup(t, db)
+	defer cleanup(t, db)
+	store := NewPGStore(db)
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			err := store.Create(tc.quote)
+			if err != tc.err {
+				t.Errorf("got %v; want %v", err, tc.err)
+			}
+		})
+	}
+}
+
+func TestGetAQuoteByID(t *testing.T) {
+	testCases := []struct {
+		desc  string
+		quote Quote
+		id    int
+		err   error
+	}{
+		{
+			desc: "get invalid id",
+			id:   0,
+			err:  sql.ErrNoRows,
+		},
+		{
+			desc:  "get success",
+			id:    1,
+			quote: Quote{Author: "ghandi", Message: "be the change"},
+			err:   nil,
+		},
+	}
+	db := getDB(t)
+	createTable(t, db)
+	seed(t, db)
+	defer cleanup(t, db)
+	store := NewPGStore(db)
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			qt, err := store.ByID(tc.id)
+			if err != tc.err {
+				t.Errorf("got %v; want %v", err, tc.err)
+			}
+			if err == nil {
+				if qt.Author != tc.quote.Author {
+					t.Errorf("got %v; want %v", err, tc.err)
+				}
+			}
+		})
+	}
 }
